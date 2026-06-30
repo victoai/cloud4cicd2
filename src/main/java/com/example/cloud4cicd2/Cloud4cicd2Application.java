1e994cabe0cea75b8b14227792407c27b450169d
victoai <victoai@naver.com>
2026-06-30 11:17:36 +0900

    github actions에서 빌드  cicd2

---
+++ src/main/java/com/example/cloud4cicd2/Cloud4cicd2Application.java
@@ -0,0 +1,13 @@
+package com.example.cloud4cicd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloud4cicd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloud4cicd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloud4cicd2/Cloud4cicd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud4cicd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cloud4cicd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
