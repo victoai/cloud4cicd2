d83f038917902b65ed9a76b5bbb9b8e0ad977374
victoai <victoai@naver.com>
2026-06-30 11:33:11 +0900

    github actions  빌드하고  jar 복사하기  cicd

--- src/main/java/com/example/cloud4cicd2/HelloController.java
+++ src/main/java/com/example/cloud4cicd2/HelloController.java
@@ -11,4 +11,9 @@ public class HelloController {
     public String hi(){
         return "hi cicd2 github action ^&&&^";
     }
+
+    @GetMapping("/hi2")
+    public String hi2(){
+        return " wjy ^^ ";
+    }
 }
