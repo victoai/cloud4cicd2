03b71e77c9fbdc01e85268dfa81452d549888a40
victoai <victoai@naver.com>
2026-06-30 11:20:35 +0900

    dd

---
+++ src/main/java/com/example/cloud4cicd2/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.cloud4cicd2;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hi cicd2 github action ^&&&^";
+    }
+}
